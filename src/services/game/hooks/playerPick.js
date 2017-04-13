888f1340d871203cef921e55fcaa38e0220cb599
JTBreunissen <jantjarco@gmail.com>
2017-04-13 21:28:50 +0200

    playerpicks get stored by hook

--- src/services/game/hooks/playerPick.js
+++ src/services/game/hooks/playerPick.js
@@ -6,28 +6,18 @@
 // For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
 
 module.exports = function(options) {
-      return function(hook) {
-            console.log(hook.data.playerPick);
-
-
-
-
-            // gets gameId from hook and use it to load game document from DB
-            return hook.app.service('games').get(hook.id)
-            .then((game) => {
-                  console.log(game.playerIds[0]);
-                  console.log(hook.params.user._id);
-                  console.log(hook.params.user._id === game.playerIds[1]);
-                  // checks if player is player 1 or 2 and stores pick in db
-
-                  if (hook.params.user._id === game.playerIds[0]) {
-                    hook.data.playerPickOne = hook.data.playerPick
-                  }
-
-                  if (hook.params.user._id === game.playerIds[1]) {
-                    hook.data.playerPickTwo = hook.data.playerPick
-                  }
-                })
-          }
-    }
-
+  return function(hook) {
+    return hook.app.service('games').get(hook.id)
+    .then((game) => {
+      if (JSON.stringify(hook.params.user._id) === JSON.stringify(game.playerIds[0])) {
+        console.log('does the first if run?');
+        hook.data.playerPickOne = hook.data.playerPick;
+        return
+      } else {
+        console.log('does the second if get run?');
+        hook.data.playerPickTwo = hook.data.playerPick;
+        return
+      }
+    })
+  }
+}
