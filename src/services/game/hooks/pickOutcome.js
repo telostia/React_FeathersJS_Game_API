07f3c5da0907810e7e77aa4529e07d0019bd3efe
florestankorp <florestankorp@gmail.com>
2017-04-14 11:21:06 +0200

    ready for monkey testing

--- src/services/game/hooks/pickOutcome.js
+++ src/services/game/hooks/pickOutcome.js
@@ -5,12 +5,29 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
 
+const gameLogic = require('../gameLogic')
 const defaults = {};
+console.log('hello from pickOutcome');
+
+
 
 module.exports = function(options) {
-  options = Object.assign({}, defaults, options);
-
   return function(hook) {
-    hook.pickOutcome = true;
-  };
+    return hook.app.service('games').get(hook.id)
+
+    .then((game) => {
+      console.log('only one player picked ' + (game.playerPickOne == '' || game.playerPickTwo == ''));
+
+      if (game.playerPickOne == '' || game.playerPickTwo == '') return
+
+
+      if (game.playerPickOne != '' && game.playerPickTwo != '') {
+        gameLogic(game.playerPickOne, game.playerPickTwo)
+
+        // console.log('p2 pick ' + game.playerPickTwo);
+        // console.log('p1 pick ' + game.playerPickOne);
+      }
+
+    })
+  }
 };
--- src/services/game/hooks/playerPick.js
+++ src/services/game/hooks/playerPick.js
@@ -10,11 +10,9 @@ module.exports = function(options) {
     return hook.app.service('games').get(hook.id)
     .then((game) => {
       if (JSON.stringify(hook.params.user._id) === JSON.stringify(game.playerIds[0])) {
-        console.log('does the first if run?');
         hook.data.playerPickOne = hook.data.playerPick;
         return;
       } else {
-        console.log('does the second if get run?');
         hook.data.playerPickTwo = hook.data.playerPick;
         return;
       }
